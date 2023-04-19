23518f32cc41adbbf7c67929b99e235660dee50a
JassonM0lina <js.molina12@uniandes.edu.co>
2023-04-19 16:20:41 -0500

    V azure ad

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,16 @@
 import NextAuth from "next-auth"
 import AzureADProvider from "next-auth/providers/azure-ad";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientId: "e56979e3-ea57-4bba-a34b-7af8cc56a41b",
+      clientSecret: "86k8Q~KmO-0Pm7rU4PO2IehH5G-FYfiedmSIgcTm",
+      tenantId: "38cfa998-f805-49cf-849a-5c8306f18c04",
     }),
   ]
 }
